45ac10ee8f1072ccfa2708fee69b849d0771659a
Чирцов Роман <rom4es_1993@mail.ru>
2020-10-09 18:36:37 +0700

    123

--- services/index.php
+++ services/index.php
@@ -6,38 +6,38 @@ $APPLICATION->SetTitle("Услуги автосалона подержанных
 <div class="b-services-page">
 	<div class="b b-services-1">
 		<div class="b-block b-text">
-			<p>Автосалон «Авто Дром» предоставляет услугу подбора подержанной машины на заказ. Подобор транспортного средства по запросу клиента на рынке с заданными параметрами и выброр наиболее выгодного варианта – основные направления работы автосалона. Менеджеры рассмотрят все предложения модели, запрошенной клиентом, выберут лучший вариант и сделают все возможное, чтобы Вы получили заказанный автомобиль как можно быстрее.<br><br>Каждая машина, предложенная менеджерами и имеющаяся в базе данных, проходит юридическую экспертизу и техническую диагностику, а перед выдачей клиенту - предпродажную подготовку. При проверке учитываются целостность кузова, наличие крашеных деталей, исправность работы двигателя, механизмов подвески и управления, общее состояние интерьера и внешнего вида транспортного средства, а также его комплектация и наличие дополнительных опций.</p>
+			<p><?=includeArea("services-text");?></p>
 		</div>
 		<div class="border-bottom-left"></div>
 	</div>
 	<div class="b b-2 b-services-2">
 		<div class="b-block">
-			<h2 class="b-title">«Авто Дром» – это удобно и безопасно</h2>
-			<h3 class="b-title">При обращении к специалистам автосалона клиент получает</h3>
+			<h2 class="b-title"><?=includeArea("services-2-title");?></h2>
+			<h3 class="b-title"><?=includeArea("services-2-subtitle");?></h3>
 			<div class="b-sale-list slider-cont mobile-slider">
 				<div class="b-sale-item">
 					<img src="<?=SITE_TEMPLATE_PATH?>/html/i/number-1.svg">
-					<h3>Безопасность сделки и оказанных услуг</h3>
+					<h3><?=includeArea("services-sale-1-title");?></h3>
 					<div class="b-sale-item-line"></div>
-					<p>Прежде всего, это касается состояния транспортного средства и чистоты документов.</p>
+					<p><?=includeArea("services-sale-1-text");?></p>
 				</div>
 				<div class="b-sale-item">
 					<img src="<?=SITE_TEMPLATE_PATH?>/html/i/number-2.svg">
-					<h3>Уверенность в приобретенном авто</h3>
+					<h3><?=includeArea("services-sale-2-title");?></h3>
 					<div class="b-sale-item-line"></div>
-					<p>Покупатель осведомляется обо всех имеющихся недочетах. Также можно ознакомиться с выбранным автомобилем на тест-драйве.</p>
+					<p><?=includeArea("services-sale-2-text");?></p>
 				</div>
 				<div class="b-sale-item">
 					<img src="<?=SITE_TEMPLATE_PATH?>/html/i/number-3.svg">
-					<h3>Экономию времени и сил</h3>
+					<h3><?=includeArea("services-sale-3-title");?></h3>
 					<div class="b-sale-item-line"></div>
-					<p>Большой опыт и помощь грамотных специалистов дает возможность быструю покупку автомобиля за короткое время.</p>
+					<p><?=includeArea("services-sale-3-text");?></p>
 				</div>
 				<div class="b-sale-item">
 					<img src="<?=SITE_TEMPLATE_PATH?>/html/i/number-4.svg">
-					<h3>Прозрачную сделку купли-продажи</h3>
+					<h3><?=includeArea("services-sale-4-title");?></h3>
 					<div class="b-sale-item-line"></div>
-					<p>Приобретая автомобиль с пробегом в автосалоне, вы попадаете под действие закона о защите прав потребителей.</p>
+					<p><?=includeArea("services-sale-4-text");?></p>
 				</div>
 			</div>
 			<div class="center mobile-nav">
@@ -56,20 +56,20 @@ $APPLICATION->SetTitle("Услуги автосалона подержанных
 		<div class="border-top-left"></div>
 		<div class="b-back-dark blue"></div>
 		<div class="b-block">
-			<h2 class="b-title white">Кроме того «Авто Дром» берет на себя  все нюансы</h2>
-			<h3 class="b-title white">Прямо в автосалоне можно оформить все необходимые документы</h3>
+			<h2 class="b-title white"><?=includeArea("services-3-title");?></h2>
+			<h3 class="b-title white"><?=includeArea("services-3-subtitle");?></h3>
 			<div class="b-advantage-list slider-cont mobile-slider">
 				<div class="b-advantage-item">
 					<img src="<?=SITE_TEMPLATE_PATH?>/html/i/b-3-img-1.svg">
-					<h3>Договор <br> купли-продажи</h3>
+					<h3><?=includeArea("services-3-1");?></h3>
 				</div>
 				<div class="b-advantage-item">
 					<img src="<?=SITE_TEMPLATE_PATH?>/html/i/b-3-img-2.svg">
-					<h3>Страховка транспортного средства</h3>
+					<h3><?=includeArea("services-3-2");?></h3>
 				</div>
 				<div class="b-advantage-item">
 					<img src="<?=SITE_TEMPLATE_PATH?>/html/i/b-3-img-3.svg">
-					<h3>Все необходимые<br> справки для ГИБДД</h3>
+					<h3><?=includeArea("services-3-3");?></h3>
 				</div>
 			</div>
 			<div class="center mobile-nav">
@@ -96,34 +96,35 @@ $APPLICATION->SetTitle("Услуги автосалона подержанных
 			</svg>
 		</div>
 	</div>
+
 	<div class="b b-2 b-services-4" id="b-services-anchor">
 		<div class="b-block">
-			<h2 class="b-title">Продаете автомобиль?</h2>
-			<h3 class="b-title">Мы так же можем упростить для вас процесс продажи!</h3>
+			<h2 class="b-title"><?=includeArea("b-2-title");?></h2>
+			<h3 class="b-title"><?=includeArea("b-2-subtitle");?></h3>
 			<div class="b-sale-list slider-cont mobile-slider">
 				<div class="b-sale-item">
 					<img src="<?=SITE_TEMPLATE_PATH?>/html/i/number-1.svg">
-					<a href="/services/vykup-avto-v-kompanii-avtodrom-bystro-prosto-i-vygodno/"><h3>Срочный выкуп <br>вашего автомобиля</h3></a>
+					<a href="/services/vykup-avto-v-kompanii-avtodrom-bystro-prosto-i-vygodno/"><h3><?=includeArea("b-2-1-title");?></h3></a>
 					<div class="b-sale-item-line"></div>
-					<p>Выкуп авто в течение<br> часа! Идеальный вариант для тех, кто не хочет ждать покупателя месяцами, пока автомобиль на рынке теряет в цене.</p>
+					<p><?=includeArea("b-2-1-text");?></p>
 				</div>
 				<div class="b-sale-item">
 					<img src="<?=SITE_TEMPLATE_PATH?>/html/i/number-2.svg">
-					<a href="/services/trade-in-v-salone-avtodrom-vozmozhnost-vygodnoy-pokupki/"><h3>Выгодный обмен <br>по программе Trade-in</h3></a>
+					<a href="/services/trade-in-v-salone-avtodrom-vozmozhnost-vygodnoy-pokupki/"><h3><?=includeArea("b-2-2-title");?></h3></a>
 					<div class="b-sale-item-line"></div>
-					<p>Обменяйте авто с пробегом на любой автомобиль из нашего салона с возмещением разницы по программе Trade-in.</p>
+					<p><?=includeArea("b-2-2-text");?></p>
 				</div>
 				<div class="b-sale-item">
 					<img src="<?=SITE_TEMPLATE_PATH?>/html/i/number-3.svg">
-					<a href="/services/komissiya-avto/"><h3>Профессиональная помощь в продаже</h3></a>
+					<a href="/services/komissiya-avto/"><h3><?=includeArea("b-2-3-title");?></h3></a>
 					<div class="b-sale-item-line"></div>
-					<p>Если у вас нет времени для продажи своего авто – поручите это профессионалам, обезопасив себя от неприятностей и обмана.</p>
+					<p><?=includeArea("b-2-3-text");?></p>
 				</div>
 				<div class="b-sale-item">
 					<img src="<?=SITE_TEMPLATE_PATH?>/html/i/number-4.svg">
-					<a href="/services/usluga-avtosalona-poderzhannykh-avtomobiley-onlayn-otsenka-avtomobiley/"><h3>Онлайн-оценка состояния автомобиля</h3></a>
+					<a href="/services/usluga-avtosalona-poderzhannykh-avtomobiley-onlayn-otsenka-avtomobiley/"><h3><?=includeArea("b-2-4-title");?></h3></a>
 					<div class="b-sale-item-line"></div>
-					<p>Даже если вы желаете продавать самостоятельно, мы поможем вам с правильным определением остаточной стоимости авто.</p>
+					<p><?=includeArea("b-2-4-text");?></p>
 				</div>
 			</div>
 			<div class="center mobile-nav">
